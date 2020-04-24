87b289967d2ac87924d15e140c03f25c4c99a903
budi152 <budibuted2@gmail.com>
2020-04-24 21:58:41 +0700

    aplikasi restaurant

---
+++ web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id12789077_tyan13');
+ define('PASS','12345');
+ define('DB','id12789077_cordova');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
